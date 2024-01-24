8d1cea35d90f68432d8e12ffaf468bf1af09e1f5
RichardAnthonyDeLeon <149099592+RichardAnthonyDeLeon@users.noreply.github.com>
2024-01-24 17:44:02 -0600

    Add files via upload

---
+++ SegundosDelDía.py
@@ -0,0 +1,10 @@
+
+
+hora = int(input("Ingresa una hora: "))
+minutos = int(input("Ingrese los minutos de la hora: "))
+
+calculo = hora * 60 *60 + (minutos * 60)
+
+
+print("Han pasado",calculo,"segundos desde que empezó el día")
+
---
+++ tipoDeDato.py
@@ -0,0 +1,6 @@
+numero = input("Ingresa un numero: ")
+
+if(type(numero)== int or type(numero) == float):
+    print("Registro con éxito") 
+else:
+    print("Ingresa un tipo de dato Float o Int")
